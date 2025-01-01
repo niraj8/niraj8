c6f24d28d443acd81974fbf8fda050ff2fc1e83d
Niraj Palecha <nirajftw@gmail.com>
2025-01-02 00:35:23 +0530

    add makefile and update script

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-npx @11ty/eleventy --input=. --output=docs --formats=md
+npx -y @11ty/eleventy --input=. --output=docs --formats=md
 mkdir -p build/images
 cp -r images/ docs/images/
 cp CNAME docs/CNAME
